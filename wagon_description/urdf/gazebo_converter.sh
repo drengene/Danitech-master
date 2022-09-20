50971923657594a8d0c060d0a89b0277dd17c1fd
Daniel Holm <dahol18@student.sdu.dk>
2022-09-20 23:32:49 +0200

    cleaned up, made gazebo wagon local

--- wagon_description/urdf/gazebo_converter.sh
+++ wagon_description/urdf/gazebo_converter.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
 file=$1
-echo ${file}
-filename=$(echo "${file}" | sed "s/.xacro//")
-echo ${filename}
-cp ${file} ${filename}_gazebo.xacro
-sed -i 's+package://wagon_description/+$(find wagon_description)/+g' ${filename}_gazebo.xacro
-xacro ${filename}_gazebo.xacro > ${filename}_gazebo.urdf
-ign sdf -p ${filename}_gazebo.urdf > ../../wagon_gazebo/models/${filename}/model.sdf
-#lineNum = grep -n "</model>" ../../wagon_gazebo/models/${filename}_gazebo.sdf
-sed -i '/model /r ../../wagon_gazebo/models/wagon/plugins.sdf' ../../wagon_gazebo/models/${filename}/model.sdf
+#echo ${file}
+
+Extension='.xacro'
+if [[ "$file" == *"$Extension"* ]]; 
+then
+
+
+    filename=$(echo "${file}" | sed "s/.xacro//")
+    #echo ${filename}
+    cp ${file} ${filename}_gazebo.xacro
+    sed -i 's+package://wagon_description/meshes+meshes+g' ${filename}_gazebo.xacro
+    sed -i 's+$(find wagon_description)/meshes+meshes+g' ${filename}_gazebo.xacro
+    xacro ${filename}_gazebo.xacro > ${filename}_gazebo.urdf
+    rm ${filename}_gazebo.xacro
+    ign sdf -p ${filename}_gazebo.urdf > ../../wagon_gazebo/models/${filename}/model.sdf
+
+    cp -r ../meshes ../../wagon_gazebo/models/${filename}/
+
+    sed -i '/model /r ../../wagon_gazebo/models/wagon/plugins.sdf' ../../wagon_gazebo/models/${filename}/model.sdf
+else
+    echo "Please give a .xacro file as input"
+
+fi
