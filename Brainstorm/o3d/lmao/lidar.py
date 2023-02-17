Rotation corrected to xyzw

Brainstorm/o3d/lmao/lidar.py
@@ -11,6 +11,8 @@ import open3d as o3d
 import numpy as np
 import matplotlib.pyplot as plt
 import quaternion
+import cv2
+from scipy.spatial.transform import Rotation as R
 
 
 class Lidar:
@@ -18,7 +20,7 @@ class Lidar:
 		self.horizontal_lines = horizontal_lines
 		self.rays_per_line = rays_per_line
 		self.vertical_fov = vertical_fov
-		self.rays = self.generate_rays(self, offset = 0)
+		self.rays = self.generate_rays(offset=0)
 
 
 	def generate_rays(self, offset = 0):
@@ -31,19 +33,50 @@ class Lidar:
 		# dz = np.cos(np.linspace((np.pi)/4, 3*np.pi/4, horizontal_lines))
 		dz = np.cos(np.linspace((np.pi)/2 - self.vertical_fov/2, (np.pi)/2 + self.vertical_fov/2, self.horizontal_lines, endpoint=True))
 		dz_scale = np.sin(np.linspace((np.pi)/2 - self.vertical_fov/2, (np.pi)/2 + self.vertical_fov/2, self.horizontal_lines, endpoint=True))
-
 		rays[:, :, 3] = rot_x * dz_scale[:, None]
 		rays[:, :, 4] = rot_y * dz_scale[:, None]
 		rays[:, :, 5] = dz[:, None]
 
 		return rays
 
-	def rotated_rays(self, rays, q):
+	def rotate_rays(self, xyzw, change_original=False):
+		# Uses Quaternion library
+		# Library uses (w, x, y, z), but the function takes (x, y, z, w)
+		# 
+		# This one is twice as fast on large arrays compared to the scipy variant
+		# At least on my machine
+		#
+		#
 		# Rotate the lidar rays into the global coordinate frame
-		rays_local_flat = rays.reshape(-1, 3)
-		rays_global_flat = quaternion.rotate_vectors(q, rays_local_flat)
-		rays_global = rays_global_flat.reshape(rays.shape)
+		q = quaternion.as_quat_array([xyzw[3], xyzw[0], xyzw[1], xyzw[2]]) # Change the order of the quaternion
+		rays_local_flat = self.rays[:,:,3:].reshape(-1, 3) # Flatten the final 3 values of the array
+		rays_global_flat = quaternion.rotate_vectors(q, rays_local_flat).astype(np.float32) # Change the data type to float32
+		# Reshape and prepend the original position to the array
+		rays_global = np.concatenate((self.rays[:, :, :3], rays_global_flat.reshape(self.rays.shape[:2] + (3,))), axis=2)
+		if change_original:
+			self.rays = rays_global
 		return rays_global
+
+	
+	def rotate_rays_scipy(self, q, change_original=False):
+		# With scipy, the quaternion is in the form (x, y, z, w)
+		rays_local_flat = self.rays[:,:,3:].reshape(-1, 3) # Flatten the final 3 values of the array
+		r = R.from_quat(q)
+		rays_global_flat = r.apply(rays_local_flat)
+		# Reshape and prepend the original position to the array
+		rays_global = np.concatenate((self.rays[:, :, :3], rays_global_flat.reshape(self.rays.shape[:2] + (3,))), axis=2)
+		if change_original:
+			self.rays = rays_global
+		return rays_global
+
+
+	def translate_rays(self, pos, change_original=False):
+		# Translate the lidar rays into the global coordinate frame
+		if change_original:
+			self.rays[:, :, :3] += pos
+			return self.rays
+		else:
+			return self.rays + [pos[0], pos[1], pos[2], 0, 0, 0]
 
 	def check_unity(self, rays = None):
 		if rays is None:
@@ -51,20 +84,25 @@ class Lidar:
 		directions = rays[:, :, 3:]
 		return np.allclose(np.linalg.norm(directions, axis=2), 1)
 
-	def plot_rays(self, rays=None, fig=None, visualize=True, plot_unit_sphere=True):
+	def plot_rays(self, rays=None,fig=None, ax=None, visualize=True, plot_unit_sphere=True):
 		if fig is None:
-			fig = plt.figure()
+			fig = plt.figure() 
+		if ax is None:
+			ax = fig.add_subplot(111, projection='3d')
 		if rays is None:
 			rays = self.rays
 		# Extract x, y, and z coordinates from the data
-		x = rays=None[:, :, 3].flatten()
-		y = rays=None[:, :, 4].flatten()
-		z = rays=None[:, :, 5].flatten()
+		x = rays[:, :, 3].flatten()
+		y = rays[:, :, 4].flatten()
+		z = rays[:, :, 5].flatten()
 
-		# Create a 3D plot
-		ax = fig.add_subplot(111, projection='3d')
-		ax.scatter(x, y, z, c='b', marker='+')
+		x0 = rays[:, :, 0].flatten()
+		y0 = rays[:, :, 1].flatten()
+		z0 = rays[:, :, 2].flatten()
 
+		# Create a 3D plot. Colors should change gradually as index increases. The very first point should be red
+		ax.scatter(x[1:]+x0[1:], y[1:]+y0[1:], z[1:]+z0[1:], c=np.arange(len(x[1:])), cmap='viridis', alpha=0.5)
+		ax.scatter(x[0]+x0[0], y[0]+y0[0], z[0]+z0[0], c='r', alpha=1)
 		if plot_unit_sphere:
 			# Plot a sphere for the origin
 			u, v = np.mgrid[0:2*np.pi:20j, 0:np.pi:10j]

Brainstorm/o3d/rayCastTest.py
@@ -1,8 +1,13 @@
 import open3d as o3d
+
 import numpy as np
 import matplotlib.pyplot as plt
-from time import time
+from time import time, sleep
 import cv2
+from lmao.world import World
+from lmao.lidar import Lidar
+import quaternion
+from scipy.spatial.transform import Rotation as R
 
 def convert_to_cv2_image(depth):
 	depth[depth == np.inf] = 0
@@ -18,10 +23,8 @@ if __name__ == "__main__":
 	cube = o3d.t.geometry.TriangleMesh.from_legacy(cube)
 	torus = o3d.geometry.TriangleMesh.create_torus().translate([0, 2, 2])
 	torus = o3d.t.geometry.TriangleMesh.from_legacy(torus)
-	sphere = o3d.geometry.TriangleMesh.create_sphere(radius=0.5).translate(
-		[0, 0, 2])
-	sphere2 = o3d.geometry.TriangleMesh.create_sphere(radius=10).translate(
-		[2, 3, 1])
+	sphere = o3d.geometry.TriangleMesh.create_sphere(radius=0.5).translate([0, 0, 2])
+	sphere2 = o3d.geometry.TriangleMesh.create_sphere(radius=10).translate([2, 3, 1])
 
 	bunny = o3d.data.BunnyMesh()
 	mesh = o3d.io.read_triangle_mesh(bunny.path).translate([0, 0, 2])
@@ -31,87 +34,77 @@ if __name__ == "__main__":
 
 	sphere = o3d.t.geometry.TriangleMesh.from_legacy(sphere)
 	sphere2 = o3d.t.geometry.TriangleMesh.from_legacy(sphere2)
-
-	scene = o3d.t.geometry.RaycastingScene()
-	scene.add_triangles(cube)
-	scene.add_triangles(torus)
-	scene.add_triangles(sphere2)
-	#scene.add_triangles(bunny)
-	_ = scene.add_triangles(sphere)
-	#print(camera_rays[0], camera_rays[1])
-
-
-
-	time0 = time()
-
-	# Create rays like os1-64.
-	horizontal_lines = 64
-	rays_per_line = 1024
-	vertical_fov = np.pi/2
-
-	# os1-64 has 64 channels in the vertical axis and 1024 rays per channel.
-	# The vertical FOV is 90 degrees, while the horizontal FOV is 360 degrees.
-	# The rays are evenly distributed in the horizontal and vertical FOV.
-	# The rays are on the form [x, y, z, dx, dy, dz][x, y, z, dx, dy, dz] ... , where the first three values are the origin of the ray, and the last three values are the direction of the ray.
-	# First, create a grid of rays
-	rays = np.zeros((horizontal_lines,rays_per_line, 6), dtype=np.float32)
-	rays_optimized = np.zeros((horizontal_lines,rays_per_line, 6), dtype=np.float32)
-
-	rot_x = np.sin(np.linspace(0, 2*np.pi, rays_per_line))
-	rot_y = np.cos(np.linspace(0, 2*np.pi, rays_per_line))
-
-	# dz = np.cos(np.linspace((np.pi)/4, 3*np.pi/4, horizontal_lines))
-	dz = np.cos(np.linspace((np.pi)/2 - vertical_fov/2, (np.pi)/2 + vertical_fov/2, horizontal_lines))
-	dz_scale = np.sin(np.linspace((np.pi)/2 - vertical_fov/2, (np.pi)/2 + vertical_fov/2, horizontal_lines))
-
-	# Instead of for loops, directly populate the rays array.
-	rays[:, :, 0] = 2
-	rays[:, :, 1] = 3
-	rays[:, :, 2] = 1
-	rays[:, :, 3] = rot_x * dz_scale[:, None]
-	rays[:, :, 4] = rot_y * dz_scale[:, None]
-	rays[:, :, 5] = dz[:, None]
-
-	# Check if the two methods are the same.
-	print("The two methods are the same: ", np.allclose(rays, rays_optimized))
-
 	
+	# Create a world.
+	world = World()
+	# Add meshes to world.
+	world.add_mesh_to_scene([cube, torus, sphere2, sphere])
 
-	# Print length of dx, dy, dz.
-	directions = rays[:, :, 3:]
-	# Check if all close
-	print("Direction vectors length for raycasting all close to 1: ",np.allclose(np.linalg.norm(directions, axis=2), 1))
-	print(np.linalg.norm(directions, axis=2))
+	# Create a lidar.
+	lidar = Lidar()
 
-	# Convert to tensor.
-	rays = o3d.core.Tensor(rays)
-	print(rays.shape)
+	# Translate rays.
+	lidar.translate_rays([2, 3, 0], change_original=True)
+	#plt.ion()
+	lidar.plot_rays(lidar.rays)
+	sleep(1)
+	# Rotate rays
+	#q = quaternion.as_quat_array([0, 0.707, 0, 0.707])
+	# q = quaternion.as_quat_array([0.707, 0.707, 0, 0]) # xyz = [pi/2, 0, 0] for quaternion library
+	# rotated = lidar.rotate_rays(q)
 
-	
-	
+	# rotated = lidar.rotate_rays_scipy([0, 0, 0.707, 0.707]) # xyz = [0, 0, pi/2] when x,y,z,w
+	# lidar.plot_rays(rotated)
+	# sleep(1)
+	# lidar.plot_rays(lidar.rays)
+	# # q = quaternion.as_quat_array([0.707,0, 0.707, 0]) # xyz = [0, pi/2, 0] for quaternion library
+	# # rotated = lidar.rotate_rays(q)
+	# rotated = lidar.rotate_rays_scipy([0, 0.707, 0, 0.707]) # xyz = [0, pi/2, 0] when x,y,z,w
+	# lidar.plot_rays(rotated)
+	# sleep(1)
+	# lidar.plot_rays(lidar.rays)
+	# # q = quaternion.from_euler_angles([ 0.707, 0, 0, 0.707]) # xyz = [0, 0, pi/2] for quaternion library
+	# # rotated = lidar.rotate_rays(q)
+	# rotated = lidar.rotate_rays_scipy([0.707, 0, 0, 0.707]) # xyz = [pi/2, 0, 0] when x,y,z,w
+	# lidar.plot_rays(rotated)
+	# sleep(1)
 
-	# ------- WARNING -------
-	# This is wrong, because the rays have length > 1, where they should have length 1.
-	# This is problematic because hit distances are calculated as the length of the ray.   
-	# I can't figure out how to normalize the rays, so I'm just going to use the camera rays for now. 
-		
-	print(rays.shape)
-	#print(rays[0], rays[1])
-	# We can directly pass the rays tensor to the cast_rays function.
-	# Time raycasting.
+
+	# Test 1000 times and get the average time.
 	t0 = time()
-	ans = scene.cast_rays(rays)
-	# plt.imshow(ans['t_hit'].numpy())
-	# plt.show()
-	print(f"Time: {time() - t0:.3f} s")
+	for _ in range(1000):
+		rotated = lidar.rotate_rays([0, 0.707, 0.707, 0])
+	t1 = time()
+	print(f"Time using quaternion library: {(t1 - t0):.3f} s")
+	print("Giving a framerate of ", 1 / ((t1 - t0) / 1000), "fps")
 
-	depth = ans['t_hit'].numpy()
-	image = convert_to_cv2_image(depth)
+	# Test 1000 times and get the average time.
+	t0 = time()
+	for _ in range(1000):
+		rotated = lidar.rotate_rays_scipy([0, 0.707, 0, 0.707]) # xyz = [0, pi/2, 0] when x,y,z,w
+	t1 = time()
+	print(f"Time using scipy: {(t1 - t0):.3f} s")
+	print("Giving a framerate of ", 1 / ((t1 - t0) / 1000), "fps")
 
-	print("Running everything took: ", time() - time0, " seconds, giving us a frequency of ", 1 / (time() - time0), "fps")
-	
-	cv2.imshow("depth", image)
-	cv2.waitKey(0)
+
+
+	exit()
+
+	t0 = time()
+	# Create a 10 second animation with fps of 10
+	for i in range(0, 500):
+		# Create quaternion from rpy
+		q = quaternion.from_euler_angles([(i/500)*2*np.pi, 0.0, 0.0]) # Roll, pitch, yaw
+		rotated = lidar.rotate_rays(q)
+		ans = world.cast_rays(rotated)
+		depth = ans['t_hit'].numpy()
+		image = convert_to_cv2_image(depth)
+		print("Frame ", i, " took ", time() - t0, " seconds, giving us a frequency of ", 1 / (time() - t0), "fps")
+		t0 = time()
+		cv2.imshow("depth", image)
+		cv2.waitKey(1)
+
+
 
 	
 