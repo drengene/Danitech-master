5eb048c8da9fd7270de7bc3896f9fc0bac0d0095
Rasmus Junge <mail@raju.dk>
2023-03-17 09:20:03 +0100

    moved functions

--- Brainstorm/o3d/lmao/mapping.py
+++ Brainstorm/o3d/lmao/mapping.py
@@ -11,33 +11,14 @@
 
 import cv2
 import numpy as np
-
-
-def remove_significant_differences_noangle(original_array, threshold):
-	# Time for remove_significant_differences_noangle:  0.9915423154830932
-	null_vector = np.zeros((3,))
-	result_array = np.copy(original_array)
-	# iterate over each point in the array
-	for i in range(1, original_array.shape[0]-1):
-		for j in range(1, original_array.shape[1]-1):
-			# calculate the distance between the normal vector of the current point and its neighbors
-			distance = np.linalg.norm(original_array[i, j] - original_array[i-1:i+2, j-1:j+2], axis=1)
-			
-			# check if all the distances are below the threshold
-			if np.all(distance < threshold):
-				result_array[i, j] = null_vector
-	return result_array
+from scipy.spatial import Delaunay
+import open3d as o3d
 
 
 def get_normals(image):
 	# Takes as input a [m,n,3] of points in 3d space.
 	# Compute the gradients in the x and y directions
-	# dx = np.gradient(image[:, :, 0]) # Returns a
-	# dy = np.gradient(image[:, :, 1])
 	dx, dy, _ = np.gradient(image)
-
-	print("dx: ", dx.shape)
-	print("dy: ", dy.shape)
 
 	# Compute the cross product of the gradients to get the normal vector
 	normal = np.cross(dx, dy)
@@ -55,3 +36,38 @@ def get_normals(image):
 	normal_image[:, :, 2] = normal[:, :, 2]
 
 	return normal_image
+
+
+def remove_significant_differences(original_array, threshold):
+	# Sets as the null vector, all the points in the array that are within a 
+	#  threshold distance from their neighbors.
+
+	# Time for remove_significant_differences_noangle:  0.9915423154830932
+	null_vector = np.zeros((3,))
+	result_array = np.copy(original_array)
+	# iterate over each point in the array
+	for i in range(1, original_array.shape[0]-1):
+		for j in range(1, original_array.shape[1]-1):
+			# calculate the distance between the normal vector of the current point and its neighbors
+			distance = np.linalg.norm(original_array[i, j] - original_array[i-1:i+2, j-1:j+2], axis=1)
+			
+			# check if all the distances are below the threshold
+			if np.all(distance < threshold):
+				result_array[i, j] = null_vector
+	return result_array
+
+
+def construct_mesh(mask, xyz, normal_image=None):
+	tri = Delaunay(np.array(np.nonzero(mask)).T)
+	vertices = np.array(xyz[tri.points[:, 0].astype(int), tri.points[:, 1].astype(int), :3] )
+		# Create a triangle mesh from the triangulation
+	mesh = o3d.geometry.TriangleMesh()
+	# Create vertices using the points from the triangulation as indeces to the "points" matrix
+	mesh.vertices = o3d.utility.Vector3dVector(vertices)
+	# Create triangles using the simplices from the triangulation
+	mesh.triangles = o3d.utility.Vector3iVector(tri.simplices)
+	# Add edges
+	if normal_image is not None:
+		# Paint the mesh in a color defined by the normals
+		mesh.vertex_colors = o3d.utility.Vector3dVector(abs(normal_image[tri.points[:, 0].astype(int), tri.points[:, 1].astype(int)]))
+	return mesh
